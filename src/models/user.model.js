a73453dfd6ad20636b58adc98b7b83d5c9c9afc9
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2024-04-25 23:30:47 +0530

    setup done for work on backend

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,49 +1,98 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import { jwt } from "jsonwebtoken";
+import bcrypt from "bcrypt"
 
-const watchHistorySchema = new mongoose.Schema({
-    videoId: {
+const userSchema = new Schema({
+    watchHistory: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Videos"
-    }
-})
-
-const userSchema = new mongoose.Schema({
-    watchHistory: [watchHistorySchema],
+    }],
     userName: {
         type: String,
         unique: true,
         required: true,
         lowercase: true,
-        trim:true,
-        index:true
+        trim: true,
+        index: true
     },
     email: {
         type: String,
         unique: true,
         required: true,
         lowercase: true,
-        trim:true,
+        trim: true,
     },
     fullName: {
         type: String,
         required: true,
-        trim:true,
-        index:true
+        trim: true,
+        index: true
     },
-    avatar:{
-        type:String,//cloudnary url
-        required:true
+    avatar: {
+        type: String,//cloudnary url
+        required: true
     },
-    coverImage:{
-        type:String,
+    coverImage: {
+        type: String,
     },
-    password:{
-        type:String,
-        required:[true,'Password is required!']
+    password: {
+        type: String,
+        required: [true, 'Password is required!']
     },
-    refreshTocken:{
-        type:String,
+    refreshTocken: {
+        type: String,
     }
 }, { timestamps: true })
+
+
+// for encription of password
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+// methods
+
+// checking password
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+// method for tocken
+userSchema.methods.generateAccessToken = async function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+            email : this.email,
+            userName : this.userName,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOCKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOCKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = async function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+        },
+        process.env.REFRESH_TOCKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOCKEN_EXPIRY
+        }
+    )
+}
+
+
+
+
+
+
+
 
 export const User = mongoose.model('User', userSchema)
