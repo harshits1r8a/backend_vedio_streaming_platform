23b2576ed1df69cfe025a740b62899d9cea7060c
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2024-09-07 01:12:09 +0530

    remove cloudinary invironment

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import connectDB from "./db/index.js";
 import { app } from './app.js';
 
 const port = process.env.PORT || 5000
-
+ 
 connectDB()
     .then(() => {
         app.listen(port, () => {
-            console.log(`⚙️ Server is running at port :${port}`);
+            console.log(`⚙️ Server is running at port : ${port}`);
         })
         app.on("error", (error) => {
-            console.log("ERROR", error);
+            console.log("ERROR (in index file for app)", error);
             throw error;  
         })
     })
