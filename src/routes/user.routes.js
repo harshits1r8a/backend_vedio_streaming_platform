ee81367a453df6ee5ff4f63f083be7648dd92cd2
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2024-11-02 14:13:14 +0530

    fix push essue

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,18 +1,14 @@
 import { Router } from "express";
-<<<<<<< HEAD
 import { loginUser, logoutUser, registerUser } from "../controllers/user.controler.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import verifyJWT from "../middlewares/auth.middleware.js";
-=======
 import { registerUser } from "../controllers/user.controler.js";
 import { upload } from "../middlewares/multer.middleware.js";
->>>>>>> 49ef271b4db1ed1a7206a364244b92323cd1eec2
 
 const router = Router()
 
 router.route("/register").post(upload.fields([
     {
-<<<<<<< HEAD
         name : "avatar",
         maxCount : 1
     },
@@ -21,18 +17,7 @@ router.route("/register").post(upload.fields([
         maxCount : 1
     }
 ]),registerUser)
-
 router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
-=======
-        name : 'avatar',
-        maxCount : 1
-    },
-    {
-        name : 'coverImage',
-        maxCount : 1
-    }
-]),registerUser)
->>>>>>> 49ef271b4db1ed1a7206a364244b92323cd1eec2
 
 export default router
